a68e40b4e52ff04268f8e3692da01c1ea14994db
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-11-30 14:33:18 +0700

    change connect to db

--- src/utils/databaseConn/sequelize.js
+++ src/utils/databaseConn/sequelize.js
@@ -1,7 +1,7 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-//CONNECT
+// //CONNECT
 // const sequelize = new Sequelize(
 //   process.env.DATABASENAME,
 //   process.env.USERNAME,
@@ -15,35 +15,35 @@ require("dotenv").config();
 //   }
 // );
 
-// const sequelize = new Sequelize('coffee', 'root', null , {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     define:{
-//       freezeTableName: true
-//     }
+// const sequelize = new Sequelize("coffeestore", "root", null, {
+//   host: "localhost",
+//   dialect: "mysql",
+//   define: {
+//     freezeTableName: true,
+//   },
 // });
 
-const sequelize = new Sequelize('coffee-store', 'admin_db', 'dbpassword@1', {
-  host: 'core-banking-db.mysql.database.azure.com',
-  dialect: 'mysql',
-  define:{
-    freezeTableName: true
-  },
-  ssl: 'ca:fs.readFileSync("{ca-cert filename}")'
-});
+// const sequelize = new Sequelize('coffee-store', 'admin_db', 'dbpassword@1', {
+//   host: 'core-banking-db.mysql.database.azure.com',
+//   dialect: 'mysql',
+//   define:{
+//     freezeTableName: true
+//   },
+//   ssl: 'ca:fs.readFileSync("{ca-cert filename}")'
+// });
 
-// const sequelize = new Sequelize(
-//   process.env.DBNAME,
-//   process.env.DBUSERNAME,
-//   process.env.DBPASSWORD,
-//   {
-//     host: process.env.DBHOST,
-//     port: process.env.DBPORT,
-//     dialect: "mysql",
-//     define: {
-//       freezeTableName: true,
-//     },
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.DBNAME,
+  process.env.DBUSERNAME,
+  process.env.DBPASSWORD,
+  {
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    dialect: "mysql",
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
