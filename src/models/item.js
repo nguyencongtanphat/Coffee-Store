f08942054652484d72ed8493ded2d74cca272180
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-12-09 08:33:47 +0700

    change db and add description to item

--- src/models/item.js
+++ src/models/item.js
@@ -5,10 +5,12 @@ class Item extends Model {}
 
 Item.init({
     Name: DataTypes.STRING,
+    Description: DataTypes.STRING,
     SPrice: DataTypes.INTEGER,
     MPrice: DataTypes.INTEGER,
     LPrice: DataTypes.INTEGER,
     Image: DataTypes.TEXT
+
 }, {sequelize, modelName:'Item'})
 
 module.exports = Item
